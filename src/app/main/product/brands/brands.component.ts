38478fd8dfdae798ccbebddeb2a71d78ac2aae19
bilalparray <parraybilal34@gmail.com>
2024-08-29 00:29:50 +0530

    brand Ui

--- src/app/main/product/brands/brands.component.ts
+++ src/app/main/product/brands/brands.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FileUploadModule } from '@iplab/ngx-file-upload';
+import { CustomizerSettingsService } from '../../../internal-components/customizer-settings/customizer-settings.service';
+import { CommonService } from '../../../services/common.service';
+import { LogHandlerService } from '../../../services/log-handler.service';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { BaseComponent } from '../../../internal-components/other-components/base.component';
+import { BrandViewModel } from '../../../models/view/end-user/brand.viewmodel';
 
 @Component({
   selector: 'app-brands',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    FileUploadModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatTooltipModule,
+    MatTableModule,
+  ],
   templateUrl: './brands.component.html',
-  styleUrl: './brands.component.scss'
+  styleUrl: './brands.component.scss',
 })
-export class BrandsComponent {
+export class BrandsComponent
+  extends BaseComponent<BrandViewModel>
+  implements OnInit
+{
+  constructor(
+    public themeService: CustomizerSettingsService,
+    commonService: CommonService,
+    logHandlerService: LogHandlerService
+  ) {
+    super(commonService, logHandlerService);
+    this.viewModel = new BrandViewModel();
+  }
+  ngOnInit(): void {}
+  // Dark Mode
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
 
+  // RTL Mode
+  toggleRTLEnabledTheme() {
+    this.themeService.toggleRTLEnabledTheme();
+  }
 }
---
+++ src/app/models/view/end-user/brand.viewmodel.ts
@@ -0,0 +1,5 @@
+import { BaseViewModel } from '../../internal/base.viewmodel';
+
+export class BrandViewModel extends BaseViewModel {
+  public multiple: boolean = false;
+}
